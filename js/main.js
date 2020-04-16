ba5d20ce47b44e14998eaf1702d9048849445585
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-16 15:54:54 +0200

    inserimento elementi da js effettuato

--- js/main.js
+++ js/main.js
@@ -1,4 +1,43 @@
 $(document).ready(function () {
-  console.log("funzia");
   
-});
+
+  //Dati
+  var todoList = [
+    "Sistemare il sito Inps",
+    "Assicurarsi che la terra non sia piatta",
+    "Scoprire dov'è Bugo",
+    "Cercare molte più Gif"
+  ];
+
+  var list = $(".todo");
+
+
+  for(var i = 0; i < todoList.length; i++){
+
+    //cloniamo la x
+    var elementList = $(".template li").clone();
+
+    //aggiungere del testo al template
+    elementList.prepend(todoList[i]);
+
+    //aggiungere il clone alla lista de
+    list.append(elementList);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+});//fine ready
